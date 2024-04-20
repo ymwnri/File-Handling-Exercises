a9ee271ac015960b2f0931f2b1e1e473c0a29f10
Aidan Tiu <tiuprof77@gmail.com>
2024-04-20 18:34:09 +0800

    Add pseudocode

---
+++ program1.py
@@ -0,0 +1,4 @@
+# Read numbers.txt that contains 20 integers
+# Create even.txt that will contain only the even numbers from numbers.txt
+# Create odd.txt that will contain only the odd numbers from numbers.txt
+# Print it in a list format
