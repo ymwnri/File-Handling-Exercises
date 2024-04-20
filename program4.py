c53aaa594a85d0e739ed8507606c4ec347b7ca1c
Aidan Tiu <tiuprof77@gmail.com>
2024-04-20 23:49:54 +0800

    Read integer.txt and convert it into a list of integer

--- program4.py
+++ program4.py
@@ -1,2 +1,14 @@
 # Create double.txt containing the square of even numbers from integers.txt
 # Create triple.txt containing the cube of odd numbers from integers.txt
+
+# Open integers.txt file
+with open('integers.txt', 'r') as numbers_file:
+
+    # Read content of integers.txt file
+    numbers = numbers_file.read()
+
+    # Split the numbers into a list of string
+    numbers = numbers.split()
+
+    # Convert the list of string to list of integer
+    numbers = [int(number) for number in numbers]
