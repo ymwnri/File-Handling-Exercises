46e8a6d329b3ac6adff68ebc89a4221785f5a0a9
Aidan Tiu <tiuprof77@gmail.com>
2024-04-20 23:40:47 +0800

    Ask user to add lines

--- program3.py
+++ program3.py
@@ -1,3 +1,11 @@
 # Write multiple line of text in mylife.txt
 # Ask for user input to add lines of text in mylife.txt
 
+# Open mylife.txt file
+with open("mylife.txt", "w") as file:
+
+    # Ask for user input to add lines of text in mylife.txt
+    while True:
+
+        # Ask the user to enter a line
+        enter_line = input("Enter line: ")
