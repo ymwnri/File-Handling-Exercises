c0624ef6ea1df622eb61bfd12266cd77726681e5
Aidan Tiu <tiuprof77@gmail.com>
2024-04-20 23:42:22 +0800

    Validate user input and store lines to mylife.txt

--- program3.py
+++ program3.py
@@ -9,3 +9,15 @@ with open("mylife.txt", "w") as file:
 
         # Ask the user to enter a line
         enter_line = input("Enter line: ")
+        
+        # Confirm if the user still wants to add another line
+        continue_adding = input("Are there more lines? (y/n)? ")
+
+        # If user input is n, write the line to the file and break
+        if continue_adding.lower() == "n":
+            file.write(enter_line + "\n")
+            break
+        
+        # If user input is y, write the line to the file
+        else:
+            file.write(enter_line + "\n")
