63c9e6af2f70ed94fb72d1cc6193072f11df8a7e
Aidan Tiu <tiuprof77@gmail.com>
2024-04-20 22:58:39 +0800

    Open student_gwa.txt and read content

---
+++ program2.py
@@ -0,0 +1,10 @@
+# Read a file that contains the name and GWA of 20 students
+# Output the name of student who got the highest GWA
+
+# Open student_gwa.txt file
+with open("student_gwa.txt", "r") as file:
+
+    # Read the content of the file
+    content = file.readlines()
+
+
