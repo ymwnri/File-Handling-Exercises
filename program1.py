a1694ba7fa08eb05cf295325bbf78bbf09c02595
Aidan Tiu <tiuprof77@gmail.com>
2024-04-20 18:40:29 +0800

    Read numbers.txt and convert it intoa list of integer

--- program1.py
+++ program1.py
@@ -2,3 +2,15 @@
 # Create even.txt that will contain only the even numbers from numbers.txt
 # Create odd.txt that will contain only the odd numbers from numbers.txt
 # Print it in a list format
+
+# Open numbers.txt
+with open('numbers.txt', 'r') as numbers_file:
+
+    # Read the intergers from the file as string
+    numbers = numbers_file.read()
+
+    # Split the numbers into a list of string
+    numbers = numbers.split()
+
+    # Convert the list of string to list of integer
+    numbers = [int(number) for number in numbers] 
