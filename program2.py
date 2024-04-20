ccd71d557eeae0377b96f1ad3fa01cb2dfd90cfa
Aidan Tiu <tiuprof77@gmail.com>
2024-04-20 23:09:02 +0800

    Create student_gwa dict and loop content of the file

--- program2.py
+++ program2.py
@@ -7,4 +7,15 @@ with open("student_gwa.txt", "r") as file:
     # Read the content of the file
     content = file.readlines()
 
+# Store the name and GWA of each student in a dictionary
+student_gwa = {}
+
+# Loop through the content of the file
+for line in content:
+
+    # Split the line into name and GWA
+    name, gwa = line.split()
+
+    # Store the name and GWA in the dictionary
+    student_gwa[name] = float(gwa)
 
